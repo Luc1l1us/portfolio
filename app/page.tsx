6cfc65572de029a8e5d36af28b2678a98fb89ecc
Luc1l1us <ikazuha001@gmail.com>
2026-02-17 20:12:40 +0800

    scrollable 2nd column, fixed height

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,18 @@ export default function Home() {
   return (
     //Change hardcoded padding and margin to scale
     //Change into 3 flex rows and scale
-    <div className="flex flex-column justify-items-start m-30 px-40 py-55 rounded-lg border border-white shadow-xl/20 text-black">
+    <div className="main-box flex flex-column justify-items-start m-20 px-30 py-20 rounded-lg border border-white shadow-xl/20 text-black">
       {/*First Column (should redirect the user to what the user had clicked)*/}
-      <div className="basis-1/3">
-        SAMPLE NAME
+      <div className="first-column basis-1/3">
+        <div>
+          SAMPLE PICTURE
+        </div>
+        edzie mari navarra
         <div>
           Software Engineer
         </div>
 
-        <div className="2nd-row mt-10">
+        <div className="navigation mt-10">
           * projects
           <div className="">
             * experience
@@ -25,13 +28,13 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="3rd-row mt-50">
+        <div className="first-bottom mt-50">
           * all rights reserved
         </div>
+      </div>
 
       {/*Second Column (should be scrollable)*/}
-      </div>
-      <div className="basis-2/3 ">
+      <div className="second-column basis-2/3 ">
         GREETINGS!
         <div className="">
           my name is SAMPLE NAME
@@ -64,6 +67,42 @@ export default function Home() {
             <div>
               3rd project name
             </div>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum."
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum."
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum."
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum."
+            </p>
           </div>
           
           {/*2nd column should be scrollable*/}
